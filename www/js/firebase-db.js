2ec76f23f910d427be07afbaafc0be1a21b469e5
lukaszpapierz93@gmail.com <kominiarka44>
2018-04-17 22:22:51 +0200

    firebase init

--- www/js/firebase-db.js
+++ www/js/firebase-db.js
@@ -1,15 +1,6 @@
 var loginUser;
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDbWNK3szFbspF1bPBMbDgBrh_Q7HnqYIw",
-    authDomain: "footballnewsletter-c366f.firebaseapp.com",
-    databaseURL: "https://footballnewsletter-c366f.firebaseio.com",
-    projectId: "footballnewsletter-c366f",
-    storageBucket: "footballnewsletter-c366f.appspot.com",
-    messagingSenderId: "1064101044778"
-};
-firebase.initializeApp(config);
+
 
 //logowanie email tymczasowow bez parametrow z formularza
 function loginEmail() {
