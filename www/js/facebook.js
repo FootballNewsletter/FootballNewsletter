7b4473dea5f9ab398a221eb76db0b6693a0ac0c2
unknown <serwinszymon@gmail.com>
2018-04-21 16:32:20 +0200

    Pobieranie danych z facebook

---
+++ www/js/facebook.js
@@ -0,0 +1,44 @@
+function buildHtmlTable() {
+ 
+ function getValue(){ //pobieranie wartości z json
+   var value= $.ajax({ 
+      url: 'https://raw.githubusercontent.com/blyskacz/JSONData/master/Real.json', //narazie adres na sztywno
+      async: false
+   }).responseText;
+   return value;
+}
+var text = getValue();
+var myList = JSON.parse(text);
+     var columns = addAllColumnHeaders(myList);
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var row$ = $('<tr/>');
+         for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+             var cellValue = myList[i][columns[colIndex]];
+ 
+             if (cellValue == null) { cellValue = ""; }
+ 
+             row$.append($('<td/>').html(cellValue));
+         }
+         $("#excelDataTable").append(row$);
+     }
+ }
+ 
+ function addAllColumnHeaders(myList)
+ {
+     var columnSet = [];
+     var headerTr$ = $('<tr/>');
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var rowHash = myList[i];
+         for (var key in rowHash) {
+             if ($.inArray(key, columnSet) == -1){
+                 columnSet.push(key);
+                 headerTr$.append($('<th/>').html(key));
+             }
+         }
+     }
+     $("#excelDataTable").append(headerTr$);
+ 
+     return columnSet;
+ }
