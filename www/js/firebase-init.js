ea00db4da75ecc06e2b57fbd452659a9693002ae
lukaszpapierz93@gmail.com <kominiarka44>
2018-05-10 19:26:36 +0200

    konfiguracja pod build

---
+++ www/js/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDbWNK3szFbspF1bPBMbDgBrh_Q7HnqYIw",
+    authDomain: "footballnewsletter-c366f.firebaseapp.com",
+    databaseURL: "https://footballnewsletter-c366f.firebaseio.com",
+    projectId: "footballnewsletter-c366f",
+    storageBucket: "footballnewsletter-c366f.appspot.com",
+    messagingSenderId: "1064101044778"
+};
+firebase.initializeApp(config);
